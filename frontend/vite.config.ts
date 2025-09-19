f6461a2a781c8a12d4373235578797ef2d93993d
DomiDre <dominique.dresen@gmail.com>
2025-09-19 20:04:01 +0200

    Use a local cert for development with https for camera access

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import fs from 'fs';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		https: {
+			key: fs.readFileSync('./local_dev.key'),
+			cert: fs.readFileSync('./local_dev.cert')
+		},
+		host: true
+	}
 });
