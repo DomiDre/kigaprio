e26ae0f2b3e099144793788006520b1c47496064
DomiDre <dominique.dresen@gmail.com>
2025-09-21 00:24:18 +0200

    Remove type complaint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,18 +2,21 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import fs from 'fs';
+import type { ServerOptions } from 'https';
 
-const useDevHTTPS = process.env.USE_DEV_HTTPS && process.env.USE_DEV_HTTPS === 'true';
+const useDevHTTPS = process.env.USE_DEV_HTTPS === 'true';
+let httpsConfig: ServerOptions | undefined = undefined;
+if (useDevHTTPS) {
+	httpsConfig = {
+		key: fs.readFileSync('./local_dev.key'),
+		cert: fs.readFileSync('./local_dev.cert')
+	};
+}
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	server: {
-		https: useDevHTTPS
-			? {
-				key: fs.readFileSync('./local_dev.key'),
-				cert: fs.readFileSync('./local_dev.cert')
-			}
-			: false,
+		https: httpsConfig,
 		host: true
 	}
 });
