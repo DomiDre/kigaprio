cbf7342cc2c89ac48eb7a48a62ba81857059715a
DomiDre <dominique.dresen@gmail.com>
2025-09-30 01:00:48 +0200

    Ignore state observer for bug finding

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -9,47 +9,47 @@
 	let { children } = $props();
 
 	// Global session observer
-	onMount(() => {
-		if (!browser) return;
-
-		let navigatingToLogin = false;
-
-		const handleStorageChange = (e: StorageEvent) => {
-			if (e.key === 'auth_token') {
-				if (!e.newValue) {
-					authStore.clearAuth();
-					if (!navigatingToLogin) {
-						navigatingToLogin = true;
-						goto('/login', { replaceState: true });
-					}
-				} else if (e.newValue && !authStore.getToken()) {
-					setTimeout(() => {
-						window.location.reload();
-					}, 100);
-				}
-			}
-		};
-
-		const originalFetch = window.fetch;
-		window.fetch = async (...args) => {
-			const response = await originalFetch(...args);
-			if (response.status === 401) {
-				authStore.clearAuth();
-				if (!navigatingToLogin) {
-					navigatingToLogin = true;
-					goto('/login', { replaceState: true });
-				}
-			}
-			return response;
-		};
-
-		window.addEventListener('storage', handleStorageChange);
-
-		return () => {
-			window.removeEventListener('storage', handleStorageChange);
-			window.fetch = originalFetch;
-		};
-	});
+	// onMount(() => {
+	// 	if (!browser) return;
+	//
+	// 	let navigatingToLogin = false;
+	//
+	// 	const handleStorageChange = (e: StorageEvent) => {
+	// 		if (e.key === 'auth_token') {
+	// 			if (!e.newValue) {
+	// 				authStore.clearAuth();
+	// 				if (!navigatingToLogin) {
+	// 					navigatingToLogin = true;
+	// 					goto('/login', { replaceState: true });
+	// 				}
+	// 			} else if (e.newValue && !authStore.getToken()) {
+	// 				setTimeout(() => {
+	// 					window.location.reload();
+	// 				}, 100);
+	// 			}
+	// 		}
+	// 	};
+	//
+	// 	const originalFetch = window.fetch;
+	// 	window.fetch = async (...args) => {
+	// 		const response = await originalFetch(...args);
+	// 		if (response.status === 401) {
+	// 			authStore.clearAuth();
+	// 			if (!navigatingToLogin) {
+	// 				navigatingToLogin = true;
+	// 				goto('/login', { replaceState: true });
+	// 			}
+	// 		}
+	// 		return response;
+	// 	};
+	//
+	// 	window.addEventListener('storage', handleStorageChange);
+	//
+	// 	return () => {
+	// 		window.removeEventListener('storage', handleStorageChange);
+	// 		window.fetch = originalFetch;
+	// 	};
+	// });
 </script>
 
 <svelte:head>
