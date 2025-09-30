064ae3a37b33eadee65d48d7be1ceef7ec1ee4de
DomiDre <dominique.dresen@gmail.com>
2025-09-30 20:00:30 +0200

    Do nothing in root page.ts

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,11 +1,5 @@
-import { browser } from '$app/environment';
-import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
-	if (browser) {
-		throw redirect(307, '/login');
-	}
-
-	return;
+	return {};
 };
