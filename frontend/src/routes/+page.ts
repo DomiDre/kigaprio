1a6628e3505acb862444c789178095407b9af601
DomiDre <dominique.dresen@gmail.com>
2025-09-30 19:59:08 +0200

    Always go to login from /

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -4,14 +4,7 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
 	if (browser) {
-		const token = localStorage.getItem('auth_token');
-		const user = localStorage.getItem('auth_user');
-
-		if (!token || !user) {
-			throw redirect(307, '/login');
-		} else {
-			throw redirect(307, '/priorities');
-		}
+		throw redirect(307, '/login');
 	}
 
 	return;
