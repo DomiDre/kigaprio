c364d4614645edeb0e8e8b80cb51d3abdee6fd80
DomiDre <dominique.dresen@gmail.com>
2025-10-16 19:14:12 +0200

    Set csp via svelte config, update dev with static docker compose

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -14,7 +14,17 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+		csp: {
+			directives: {
+				'script-src': ['self']
+			},
+			// must be specified with either the `report-uri` or `report-to` directives, or both
+			reportOnly: {
+				'script-src': ['self'],
+				'report-uri': ['/']
+			}
+		}
 	}
 };
 
