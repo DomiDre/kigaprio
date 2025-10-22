555ba12e0bf90b83f164d539b3ef14a72ecf6f1c
DomiDre <dominique.dresen@gmail.com>
2025-10-22 23:41:10 +0200

    Remove tests of fastapi until it is done proper

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,10 +1,1 @@
-import pytest
-from fastapi.testclient import TestClient
 
-from kigaprio.main import app
-
-
-@pytest.fixture(scope="function")
-def client():
-    with TestClient(app) as test_client:
-        yield test_client
